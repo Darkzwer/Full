6eae3dc1c81304e2ec79860c568a40c66e14c06a
Darkzwer123 <137776351+Darkzwer123@users.noreply.github.com>
2024-05-31 16:34:02 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Full
+
+A description of this package.
---
+++ Tests/FullTests/FullTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Full
+
+final class FullTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Full().text, "Hello, World!")
+    }
+}
