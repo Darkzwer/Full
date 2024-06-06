c93dafcf4423c26fb89f90c9fb224cacf5f72460
Darkzwer123 <137776351+Darkzwer123@users.noreply.github.com>
2024-06-06 14:53:42 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,16 +10,16 @@ let package = Package(
         .library(name: "DocumentReaderSDK", targets: ["DocumentReaderTarget", "Full"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Darkzwer/CommonDepSwift.git", from: "7.1.0"),
+        .package(url: "https://github.com/Darkzwer/CommonDepSwift.git", from: "7.2.0"),
     ],
     targets: [
         .target(name: "Full", dependencies: [
             .product(name: "RegulaCommonSwift", package: "CommonDepSwift")
         ]),
         .binaryTarget(
-                    name: "DocumentReaderTarget",
-                    url: "https://pods.regulaforensics.com/DocumentReader/7.1.3379/DocumentReader-7.1.3379.zip",
-                    checksum: "24f3ab840a064180a0e72ad3e22bd260e2595ecd8a4ec20b2e32b01965a9f976"),
+            name: "DocumentReaderTarget",
+            url: "https://pods.regulaforensics.com/DocumentReader/7.2.3545/DocumentReader-7.2.3545.zip",
+            checksum: "833b43dba07afceed74e14e8cd6c4c5fb41055da987164ee047f195f4577ab6d"),
         .testTarget(
             name: "FullTests",
             dependencies: ["Full"]),
